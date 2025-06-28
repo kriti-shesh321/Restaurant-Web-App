4f946ab11172ee11db290a9870f525c4e2c532bd
kriti <kritishrivastav17@gmail.com>
2025-06-29 03:13:14 +0530

    modified base url for prod

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "/api/v1",
+  baseURL: "https://restaurant-web-app-production.up.railway.app" || "/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 
